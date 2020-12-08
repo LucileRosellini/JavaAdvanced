707f3a731048d69f6fdb8122747d1ad26c0e0da7
Lucile ROSELLINI <l.rosellini@cerberis.com>
2020-12-08 13:10:19 +0100

    Creation constructeur character et listeCharacter

--- src/main/java/com/ecommerce/microcommerce/model/Character.java
+++ src/main/java/com/ecommerce/microcommerce/model/Character.java
@@ -2,12 +2,49 @@ package com.ecommerce.microcommerce.model;
 
 public class Character {
 
+    private String name ;
+    private int ID;
+    private boolean type;
+
+
     //ICI Declaration des attributs de character
-            //ID NAME CLASSE PERSO
-
+    //ID NAME CLASSE PERSO
     //getter setter
-
     //constructeur avec 0 arguments ==> Convention . On définit toujours un constructeur vide.
     //constructeur avec 3 arguments
 
+
+    public Character (String nomJoueur, int Identifiant , boolean typeWarrior ) {
+        this.name = nomJoueur;
+        this.ID = Identifiant;
+        this.type = typeWarrior;
+    }
+
+
+    public String getName() {
+        return(this.name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return (this.ID);
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public boolean getType() {
+        return (this.type);
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
 }
+
+
+
--- src/main/java/com/ecommerce/microcommerce/serviceDAO/CharacterBDD.java
+++ src/main/java/com/ecommerce/microcommerce/serviceDAO/CharacterBDD.java
@@ -8,7 +8,38 @@ public class CharacterBDD {
 
     public ArrayList<Character> ListCharacter(){
         ArrayList <Character> ListCharacter=new ArrayList<Character>();
-        ListCharacter.add(new Character());
+        ListCharacter.add(new Character("Gandalf", 0, false));
+        ListCharacter.add(new Character("Melchior", 1, false));
+        ListCharacter.add(new Character("Chaussette", 2, false));
+        ListCharacter.add(new Character("Edmund", 3, false));
+        ListCharacter.add(new Character("Tinky", 4, false));
+        ListCharacter.add(new Character("Agatha", 5, false));
+        ListCharacter.add(new Character("Elminster", 6, false));
+        ListCharacter.add(new Character("Tara", 7, false));
+        ListCharacter.add(new Character("Mandrakef", 8, false));
+        ListCharacter.add(new Character("Merlin", 9, false));
+        ListCharacter.add(new Character("Wendy", 10, false));
+        ListCharacter.add(new Character("Misstic", 11, false));
+        ListCharacter.add(new Character("Black", 12, false));
+        ListCharacter.add(new Character("Pixie", 13, false));
+        ListCharacter.add(new Character("Viviane", 14, false));
+
+        ListCharacter.add(new Character("Beowulf", 15, true));
+        ListCharacter.add(new Character("Ganthar", 16, true));
+        ListCharacter.add(new Character("Muther", 17, true));
+        ListCharacter.add(new Character("Sheera", 18, true));
+        ListCharacter.add(new Character("Entrapta", 19, true));
+        ListCharacter.add(new Character("Demonia", 20, true));
+        ListCharacter.add(new Character("Isildur", 21, true));
+        ListCharacter.add(new Character("Catra", 22, true));
+        ListCharacter.add(new Character("Elisabeth", 23, true));
+        ListCharacter.add(new Character("Fleche", 24, true));
+        ListCharacter.add(new Character("Olween", 25, true));
+        ListCharacter.add(new Character("Thalos", 26, true));
+        ListCharacter.add(new Character("Nessa", 27, true));
+        ListCharacter.add(new Character("Ragnor", 28, true));
+        ListCharacter.add(new Character("Lilith", 29, true));
+
         return ListCharacter;
     }
 }
